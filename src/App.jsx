e7ac53114c631c7f2ee688c0539a766aaaccbb2f
Elias Juarez <juarezelias83@gmail.com>
2024-02-05 02:14:31 -0300

    se agrego la hoja de estilo con un background y se creo el componente Container con un titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-
-
   return (
-    <section></section>
-  )
+    <Container className="my-4">
+      <h1 className="text-center">Bienvenido</h1>
+    </Container>
+  );
 }
 
-export default App
+export default App;
