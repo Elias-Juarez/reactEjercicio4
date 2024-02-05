de3a0a3485f877ea6afc32e6f935b0a8c1bc99d5
Elias Juarez <juarezelias83@gmail.com>
2024-02-05 04:36:27 -0300

    se muestra el nombre de la tarea en itemTarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -34,7 +34,7 @@ const FormularioTarea = () => {
           </Button>
         </Form.Group>
       </Form>
-      <ListaTareas></ListaTareas>
+      <ListaTareas tareas={tareas}></ListaTareas>
     </section>
   );
 };
