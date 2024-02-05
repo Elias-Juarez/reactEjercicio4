9be0e3563b11417b5516809ea48923cf7ae1c61b
Elias Juarez <juarezelias83@gmail.com>
2024-02-05 02:52:02 -0300

    se agrego el required en el input del formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,13 +1,24 @@
-import { Button } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 const FormularioTarea = () => {
   return (
     <section>
       <Form>
-        <Form.Group className="mb-3 d-flex" controlId="exampleForm.ControlInput1">
-          <Form.Control type="text" placeholder="Ej: Tarea 1" minLength={3} maxLength={50} required/>
-          <Button variant='dark' className='ms-3' type='submit'>Agregar</Button>
+        <Form.Group
+          className="mb-3 d-flex"
+          controlId="exampleForm.ControlInput1"
+        >
+          <Form.Control
+            type="text"
+            placeholder="Ej: Tarea 1"
+            minLength={3}
+            maxLength={50}
+            required
+          />
+          <Button variant="dark" className="ms-3" type="submit">
+            Agregar
+          </Button>
         </Form.Group>
       </Form>
     </section>
